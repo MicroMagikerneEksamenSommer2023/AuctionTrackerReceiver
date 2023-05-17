7d07f557da40a502167e6955f922bd7baa0ff805
William Kruse <williamkruse4@gmail.com>
2023-05-17 22:05:29 +0200

    gitignore added

--- AuctionTrackerReceiver/Program.cs
+++ AuctionTrackerReceiver/Program.cs
@@ -1,3 +1,10 @@
+using NLog;
+using NLog.Web;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
+{
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Logging.ClearProviders();
+//services her
+builder.Host.UseNLog();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +32,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+NLog.LogManager.Shutdown();
+}
+
